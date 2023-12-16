0810d23d6997966b90e8009e8d99b4b6ed2b60d9
Joshua <rodrigues.joshr@gmail.com>
2023-12-16 21:36:23 +0530

    editable nodes

--- components/Nodes/CharacterNode.tsx
+++ components/Nodes/CharacterNode.tsx
@@ -1,13 +1,37 @@
-import React from "react";
-import { Handle } from "reactflow";
+import { Store, StoreData, useStore } from "@/store/store";
+import React, { ChangeEvent, useState } from "react";
+import { Handle, Position } from "reactflow";
+import { Input } from "../ui/input";
+import { shallow } from "zustand/shallow";
 
-const CharacterNode = () => {
+const selector = (id: string) => (store: Store) => ({
+  setCharacterLabel: (e: ChangeEvent<HTMLInputElement>) =>
+    store.updateNode(id, { label: e.target.value }),
+});
+
+const CharacterNode = ({ id, data }: { id: string; data: StoreData }) => {
+  const { setCharacterLabel } = useStore(selector(id), shallow);
+
+  const [editMode, setEditMode] = useState(false);
+
   return (
-    <div className=" bg-primary-foreground w-32 text-center text-primary shadow-md rounded-sm">
-      <Handle position="top" type="target" />
-      <p>test</p>
-      <Handle position={"bottom"}type="source" />
-    </div>
+
+   
+
+      <div
+        onBlur={() => setEditMode(false)}
+        className=" bg-primary-foreground w-32 text-center text-primary shadow-md rounded-sm"
+      >
+        <Handle position={"top" as Position}type="target" />
+     
+        {editMode ? (
+          <Input value={data.label} onChange={setCharacterLabel} />
+        ) : (
+          <p onClick={() => setEditMode(true)}>{data.label}</p>
+        )}
+        <Handle position={"bottom" as Position} type="source" />
+      </div>
+   
   );
 };
 
--- store/store.ts
+++ store/store.ts
@@ -1,44 +1,56 @@
 import { Edge, Node } from "reactflow";
 import { createWithEqualityFn } from "zustand/traditional";
 
-
-export type Store ={
-    nodes:Node[],
-    edges:Edge[]
+export type StoreData ={
+    label:string
 }
 
+export type Store = {
+  nodes: Node[];
+  edges: Edge[];
+  updateNode: (id: string, data: StoreData) => void;
+};
 
-export const useStore = createWithEqualityFn<Store>((set,get)=>({
-    nodes:[
-        {
-          id: '1',
-          type:"char",
-          data: { label: 'Never' },
-          position: { x: 100, y: 15 },
-        },
-      
-        {
-          id: '2',
-          type:"char",
-          data: { label: 'Forget' },
-          position: { x: 100, y: 25 },
-        },
-        {
-          id: '3',
-          type:"char",
-          data: { label: 'a' },
-          position: { x: 100, y: 35 },
-        },
-        {
-          id: '4',
-          type:"char",
-          data: { label: 'Character' },
-          position: { x: 100, y: 45 },
-        },
-      ],
-    edges:[
-        { id: 'e1-2', source: '1', target: '2' },
-        { id: 'e2-3', source: '2', target: '3' },
-        { id: 'e3-4', source: '3', target: '4' },
-      ]
-}))
+export const useStore = createWithEqualityFn<Store>((set, get) => ({
+  nodes: [
+    {
+      id: "1",
+      type: "char",
+      data: { label: "Never" },
+      position: { x: 100, y: 15 },
+    },
+
+    {
+      id: "2",
+      type: "char",
+      data: { label: "Forget" },
+      position: { x: 100, y: 25 },
+    },
+    {
+      id: "3",
+      type: "char",
+      data: { label: "a" },
+      position: { x: 100, y: 35 },
+    },
+    {
+      id: "4",
+      type: "char",
+      data: { label: "Character" },
+      position: { x: 100, y: 45 },
+    },
+  ],
+  edges: [
+    { id: "e1-2", source: "1", target: "2" },
+    { id: "e2-3", source: "2", target: "3" },
+    { id: "e3-4", source: "3", target: "4" },
+  ],
+  updateNode(id, data) {
+    set({
+      nodes: get().nodes.map((node) =>
+        node.id === id
+          ? { ...node, data: Object.assign(node.data, data) }
+          : node
+      ),
+    });
+  },
+}));
