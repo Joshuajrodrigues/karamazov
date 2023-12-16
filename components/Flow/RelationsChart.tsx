963642d8f99b8105e1a4458bb35626d299278ab0
Joshua <rodrigues.joshr@gmail.com>
2023-12-16 22:54:19 +0530

    mini map added

--- components/Flow/RelationsChart.tsx
+++ components/Flow/RelationsChart.tsx
@@ -17,15 +17,9 @@ import ReactFlow, {
   Background,
   Controls,
   Panel,
+  MiniMap,
 } from "reactflow";
 
-const fitViewOptions: FitViewOptions = {
-  padding: 0.2,
-};
-
-const defaultEdgeOptions: DefaultEdgeOptions = {
-  animated: true,
-};
 import "reactflow/dist/style.css";
 
 import Header from "../Header/Header";
@@ -34,54 +28,52 @@ import CharacterNode from "../Nodes/CharacterNode";
 import { Store, useStore } from "@/store/store";
 import { shallow } from "zustand/shallow";
 
+const fitViewOptions: FitViewOptions = {
+  padding: 0.2,
+};
 
-const nodeTypes ={
-  char:CharacterNode
-}
+const defaultEdgeOptions: DefaultEdgeOptions = {
+  animated: true,
+};
+const proOptions = { hideAttribution: true };
+const nodeTypes = {
+  char: CharacterNode,
+};
 
-const selector=(store:Store)=>({
-  nodes:store.nodes,
-  edges:store.edges,
-  createNode:store.createNode,
-  onNodesChange:store.onNodesChange,
-  onEdgeChange:store.onEdgeChange,
-  addEdge:store.addEdge
-})
-
+const selector = (store: Store) => ({
+  nodes: store.nodes,
+  edges: store.edges,
+  createNode: store.createNode,
+  onNodesChange: store.onNodesChange,
+  onEdgeChange: store.onEdgeChange,
+  addEdge: store.addEdge,
+});
 
 const RelationsChart = () => {
-  
-  const store = useStore(selector,shallow)
-
-  const [edges, setEdges] = useState<Edge[]>(store.edges);
-
-
-
-
+  const store = useStore(selector, shallow);
 
   return (
-
-      <ReactFlow
-        nodeTypes={nodeTypes}
-        nodes={store.nodes}
-        edges={store.edges}
-        onNodesChange={store.onNodesChange}
-        onEdgesChange={store.onEdgeChange}
-        onConnect={store.addEdge}
-        fitView
-        fitViewOptions={fitViewOptions}
-        defaultEdgeOptions={defaultEdgeOptions}
-      >
-        <Background />
-       
-        <Panel style={{ width: "100%", margin: "0" }} position="top-center">
-          <Header />
-        </Panel>
-        <Panel position="top-right">
-          <Button onClick={()=>store.createNode()}>Add</Button>
-        </Panel>
-      </ReactFlow>
-   
+    <ReactFlow
+      nodeTypes={nodeTypes}
+      nodes={store.nodes}
+      edges={store.edges}
+      onNodesChange={store.onNodesChange}
+      onEdgesChange={store.onEdgeChange}
+      onConnect={store.addEdge}
+      fitView
+      proOptions={proOptions}
+      fitViewOptions={fitViewOptions}
+      defaultEdgeOptions={defaultEdgeOptions}
+    >
+      <Background />
+      <MiniMap zoomable pannable />
+      <Panel style={{ width: "100%", margin: "0" }} position="top-center">
+        <Header />
+      </Panel>
+      <Panel position="top-right">
+        <Button onClick={() => store.createNode()}>Add</Button>
+      </Panel>
+    </ReactFlow>
   );
 };
 
